b2b723f981c79b375dbe7dc75082f53bfa2d5dd6
kibuchi muchai <me@kibuchimuchai>
2021-10-17 17:16:35 +0300

    added functions to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function calculate(){
+    var number = document.getElementById("age").value;
+
+    if(number == "" || number == null){
+        window.alert("Please enter valid Age")
+        return;
+    }
+}
